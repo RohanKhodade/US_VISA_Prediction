bfcf5bbdddf4921b68744b9f85fea397d815fa79
RohanKhodade <rohankhodade883@gmail.com>
2025-01-16 16:35:58 +0530

    saved knn_model and preprocessor

---
+++ src/constants/__init__.py
@@ -0,0 +1,27 @@
+import os
+from datetime import date
+
+DATABASE_NAME="US_VISA"
+COLLECTION_NAME="visa_data"
+MONGODB_URL_KEY="MONGODB_URL"
+PIPELINE_NAME:str="usvisa"
+ARTIFACT_DIR:str="artifact"
+MODEL_FILE_NAME="model.pkl"
+
+
+TRAIN_FILE_NAME:str="train.csv"
+TEST_FILE_NAME:str="test.csv"
+
+FILE_NAME:str="usvisa.csv"
+
+
+
+
+
+
+DATA_INGESTION_COLLECTION_NAME:str="visa_data"
+DATA_INGESTION_DIR_NAME:str="data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR:str="feature_store"
+DATA_INGESTION_INGESTED_DIR:str="ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_DIR:float=0.2
+
