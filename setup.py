e3b50d534cbdddd1f587bb12a92372817d7bd721
RohanKhodade <rohankhodade883@gmail.com>
2025-01-17 19:56:17 +0530

    deleted unwanted

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="US_VISA_Prediction",
-    version="0.0.0",
-    author="RohanKhodade",
-    author_email="rohankhodade883@gmail.com",
-    packages=find_packages()
-)
